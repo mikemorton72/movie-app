61b0e5a165b1f4b05180bf8b24bf81a1f8d17555
mike <mmorton7295@gmail.com>
2021-06-22 12:52:37 -0500

    moved Actor controller methods into correct ruby file. RESTful routing conventions.

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,38 @@
 class ActorsController < ApplicationController
+  def index
+    render json: Actor.all.as_json
+  end
+
+  def show
+    actor_id = params[:id]
+    actor = Actor.find_by(id: actor_id)
+    render json: actor.as_json
+  end
+
+  def create
+    actor = Actor.new(
+      first_name: params[:input_first_name],
+      last_name: params[:input_last_name],
+      known_for: params[:input_known_for]
+    )
+    actor.save
+    render json: actor.as_json
+  end
+  
+  def update
+    actor_id = params[:id]
+    actor = Actor.find_by(id: actor_id)
+    actor.first_name = params[:input_first_name]
+    actor.last_name = params[:input_last_name]
+    actor.known_for = params[:input_known_for]
+    actor.save
+    render json: actor.as_json
+  end
+
+  def destroy
+    actor_id = params[:id]
+    actor = Actor.find_by(id: actor_id)
+    render json: actor.as_json
+    actor && actor.destroy
+  end
 end
